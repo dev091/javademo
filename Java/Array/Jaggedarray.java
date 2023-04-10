29415ee84025ba03c9c15f8e41c830c7b16b36f5
dev091 <rahuldev2496440@gmail.com>
2023-04-10 15:14:40 -0400

    jaggedArray

---
+++ Java/Array/Jaggedarray.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Jaggedarray {
+    public static void main(String[] args) {
+        int a[][] = {{5,6,2}, // row 0
+                     {7,9,8,4},   // 8 = row 1 column 2
+                     {3,4,2,1,5},        // row 2
+                     {5,4,7,0}}; // row 3
+        System.out.println(a[3][1]); // row - column
+    }
+}
+
+    
--- Java/Array/TwoDArray.java
+++ Java/Array/TwoDArray.java
@@ -2,10 +2,10 @@ package Array;
 
 public class TwoDArray {
     public static void main(String[] args) {
-        int a[][] = {{5,6,2,1}, // row 1
+        int a[][] = {{5,6,2,1}, // row 0
                      {7,9,8,4},   // 8 = row 1 column 2
                      {3,4,2,1},
                      {5,4,7,0}}; // row 2
-        System.out.println(a[1][3]); // row - column
+        System.out.println(a[0][1]); // row - column
     }
 }
