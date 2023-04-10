64619b348dc52d1dec33e6e2b124d91d6f83b8be
dev091 <rahuldev2496440@gmail.com>
2023-04-10 19:06:08 -0400

    1

--- Java/Array/TwoDArray.java
+++ Java/Array/TwoDArray.java
@@ -6,7 +6,7 @@ public class TwoDArray {
                      {7,9,8,4},   // 8 = row 1 column 2
                      {3,4,2,1},
                      {5,4,7,0}}; // row 2
-        System.out.println(a[0][1]); // row - column
+        System.out.println(a[0][2]); // row - column
         System.out.println("length of array is: "+a.length); 
         System.out.println("total elements of array is: "+a.length*a[0].length);  // total rows * total columns
 
