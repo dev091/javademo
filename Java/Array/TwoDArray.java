3f93b612e3596868dee999055c6eb2c55b466f1b
dev091 <rahuldev2496440@gmail.com>
2023-04-10 14:42:24 -0400

    2D Array

---
+++ Java/Array/TwoDArray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int a[][] = {{5,6,2,1},
+                     {7,9,8,4},
+                     {3,4,2,1},
+                     {5,4,7,0}};
+        System.out.println(a[1][2]);
+    }
+}
