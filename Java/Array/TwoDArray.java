4646b2905d46fbf21dd8519b1a1049b8b19de1a7
dev091 <rahuldev2496440@gmail.com>
2023-04-10 14:52:45 -0400

    comments

--- Java/Array/TwoDArray.java
+++ Java/Array/TwoDArray.java
@@ -2,10 +2,10 @@ package Array;
 
 public class TwoDArray {
     public static void main(String[] args) {
-        int a[][] = {{5,6,2,1},
-                     {7,9,8,4},
+        int a[][] = {{5,6,2,1}, // row 1
+                     {7,9,8,4},   // 8 = row 1 column 2
                      {3,4,2,1},
-                     {5,4,7,0}};
-        System.out.println(a[1][2]);
+                     {5,4,7,0}}; // row 2
+        System.out.println(a[1][31]); // row - column
     }
 }
