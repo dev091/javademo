15faa8468205489a5a0e4fb031714d03d71fa87d
dev091 <rahuldev2496440@gmail.com>
2023-04-10 19:30:10 -0400

    1

--- Java/Array/AnonymousArray.java
+++ Java/Array/AnonymousArray.java
@@ -1,5 +1,22 @@
 package Array;
 
+class Calc {
+    public int add(int num[]) // method
+    {
+        int result = 0;
+        for (int n : num) {
+            result = result + n;
+        }
+        return result;
+    }
+
+}
+
 public class AnonymousArray {
-    
+    public static void main(String[] args) {
+
+        Calc obj = new Calc();
+        int result = obj.add(new int[] { 5, 6, 4, 3 });
+        System.out.println(result);
+    }
 }
