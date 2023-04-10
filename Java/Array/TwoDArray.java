98545721a1f0fa8db4a9ed6af8dfdd31a8210442
dev091 <rahuldev2496440@gmail.com>
2023-04-10 15:14:04 -0400

    2darray

--- Java/Array/TwoDArray.java
+++ Java/Array/TwoDArray.java
@@ -6,6 +6,6 @@ public class TwoDArray {
                      {7,9,8,4},   // 8 = row 1 column 2
                      {3,4,2,1},
                      {5,4,7,0}}; // row 2
-        System.out.println(a[1][31]); // row - column
+        System.out.println(a[1][3]); // row - column
     }
 }
