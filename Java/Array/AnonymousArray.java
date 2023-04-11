fe84ffcbc67f24f08c564e5b794ed679cb29e2bf
dev091 <rahuldev2496440@gmail.com>
2023-04-10 22:36:15 -0400

    1

--- Java/Array/AnonymousArray.java
+++ Java/Array/AnonymousArray.java
@@ -18,5 +18,8 @@ public class AnonymousArray {
         Calc obj = new Calc();
         int result = obj.add(new int[] { 5, 6, 4, 3 });
         System.out.println(result);
+
+        int a []= {22,34,45,64};
+        System.out.println(a[a.length-1]); // to print the last value
     }
 }
