6f570ba5b12ff3b7fdd735fc325dcc3ba6b7ea2e
dev091 <rahuldev2496440@gmail.com>
2023-04-23 17:20:58 -0400

    Updatearray

---
+++ Java/Array/Updatearray.java
@@ -0,0 +1,10 @@
+package Array;
+import java.util.Arrays;
+
+public class Updatearray {
+    public static void main(String[] args) {
+        String[] a = {"mohan", "ramesh","radha","vikram"};
+        a[0] = "chandan";
+        System.out.println(Arrays.toString(a));
+    }
+}
