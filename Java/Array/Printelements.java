c86f7f9523c6f1d10204eb95f0a453ae0e4c88c9
dev091 <rahuldev2496440@gmail.com>
2023-04-25 17:08:43 -0400

    printelements

---
+++ Java/Array/Printelements.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Printelements {
+    public static void main(String[] args) {
+        int []x = {2,3,9,8,7};
+        int n = x.length;
+        for( int i=4 ; i>=0 ; i--){
+            
+            System.out.print(x[i]); // 
+        }
+        System.out.println();
+    }
+    
+}
